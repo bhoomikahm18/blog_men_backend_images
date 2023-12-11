f21001a06d3f0db0865444331aa25aa7d916d0f3
Bhoomika <bhoomikahm18@gmail.com>
2023-12-11 11:13:43 +0530

    Building connection to backend

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user-routes.js');
+const blogRouter = require('./routes/blog-routes.js');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
+
+mongoose.connect("mongodb+srv://bhoomikahm18:bhoomi123@cluster0.5qlvwpp.mongodb.net/Blog?retryWrites=true&w=majority")
+    .then(() => app.listen(5000))
+    .then(() => console.log("Connected to Database and listening to localhost 5000"))
+    .catch((err) => console.log(err))
